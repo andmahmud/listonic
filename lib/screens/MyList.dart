365c06c58f96c714860259a2bb35221857536fd6
MD.MAHMUDUL HASAN <126802456+andmahmud@users.noreply.github.com>
2024-12-29 15:50:20 +0600

    update

---
+++ lib/screens/MyList.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class Mylist extends StatelessWidget {
+  const Mylist({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,65 +1,48 @@
 import 'package:flutter/material.dart';
-import 'package:listonic/screens/AllProductsPage%20.dart';
-import 'package:listonic/screens/Done_work.dart';
-import 'package:listonic/screens/drawar.dart'; // Custom drawer
-import 'package:listonic/screens/my_list.dart'; // Task list screen
+
+import 'package:listonic/screens/Food%20_Facts.dart';
+import 'package:listonic/screens/MyList.dart';
+import 'package:listonic/screens/Profile.dart';
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key});
 
   @override
-  // ignore: library_private_types_in_public_api
   _MyHomePageState createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _currentIndex = 0; // Tracks the current selected index
+  int _currentIndex = 0;
 
-  // List of pages to display for each BottomNavigationBar item
   final List<Widget> _pages = [
-    MyList(), // Using the MyList widget for the Home page
-    DoneWorkList(),
-
-    AllProductsPage(),
+    Mylist(),
+    FoodFacts(),
+    Profile(),
   ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      drawer: drawar(), // Custom drawer
-      appBar: AppBar(
-        title: Text(
-          _currentIndex == 0
-              ? "My List"
-              : _currentIndex == 1
-                  ? "Search"
-                  : "Profile", // Fixed ternary operator syntax
-        ),
-        backgroundColor:
-            Colors.transparent, // Transparent background to remove extra UI
-        elevation: 0, // No shadow for the AppBar
-      ),
-
-      body: _pages[_currentIndex], // Display page based on selected index
+      body: _pages[_currentIndex], // Page content
       bottomNavigationBar: BottomNavigationBar(
-        currentIndex: _currentIndex, // Highlight the current index
+        currentIndex: _currentIndex,
         onTap: (index) {
           setState(() {
-            _currentIndex = index; // Update the index when an item is tapped
+            _currentIndex = index;
           });
         },
-        items: [
+        items: const [
           BottomNavigationBarItem(
-            icon: Icon(Icons.home),
-            label: "Home",
+            icon: Icon(Icons.list_alt),
+            label: "List",
           ),
           BottomNavigationBarItem(
-            icon: Icon(Icons.done_all),
-            label: "Done Work",
+            icon: Icon(Icons.food_bank),
+            label: "Food Facts",
           ),
           BottomNavigationBarItem(
-            icon: Icon(Icons.production_quantity_limits),
-            label: "All Products",
+            icon: Icon(Icons.person),
+            label: "Profile",
           ),
         ],
         selectedItemColor: Colors.black,
