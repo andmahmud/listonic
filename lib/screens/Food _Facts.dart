81cfa5dd22a4a142d5bca98130538316eb5cb029
MD.MAHMUDUL HASAN <126802456+andmahmud@users.noreply.github.com>
2024-12-29 19:04:47 +0600

    update

--- lib/screens/Food _Facts.dart
+++ lib/screens/Food _Facts.dart
@@ -1,8 +1,59 @@
-// ignore: file_names
 import 'package:flutter/material.dart';
 
-class FoodFacts extends StatelessWidget {
+class FoodFacts extends StatefulWidget {
   const FoodFacts({super.key});
+
+  @override
+  _FoodFactsState createState() => _FoodFactsState();
+}
+
+class _FoodFactsState extends State<FoodFacts> {
+  final TextEditingController _searchController = TextEditingController();
+  List<Map<String, String>> foodItems = [
+    {'name': 'Ale beer', 'image': 'assets/images/favicon.png'},
+    {'name': 'Alfredo sauce', 'image': 'assets/images/favicon.png'},
+    {'name': 'Allspice', 'image': 'assets/images/favicon.png'},
+    {'name': 'Almond milk', 'image': 'assets/images/favicon.png'},
+    {'name': 'Almonds', 'image': 'assets/images/favicon.png'},
+    {'name': 'American ', 'image': 'assets/images/favicon.png'},
+    {'name': 'Apple', 'image': 'assets/images/favicon.png'},
+    {'name': 'Avocado', 'image': 'assets/images/favicon.png'},
+    {'name': 'Banana', 'image': 'assets/images/favicon.png'},
+    {'name': 'Carrot', 'image': 'assets/images/favicon.png'},
+    {'name': 'Celery', 'image': 'assets/images/favicon.png'},
+    {'name': 'Chia seeds', 'image': 'assets/images/favicon.png'},
+    {'name': 'Chili peppers', 'image': 'assets/images/favicon.png'},
+    {'name': 'Coconut milk', 'image': 'assets/images/favicon.png'},
+    {'name': 'Cucumber', 'image': 'assets/images/favicon.png'},
+    {'name': 'Garlic', 'image': 'assets/images/favicon.png'},
+    {'name': 'Lemon', 'image': 'assets/images/favicon.png'},
+  ];
+
+  List<Map<String, String>> filteredItems = [];
+
+  @override
+  void initState() {
+    super.initState();
+    filteredItems = foodItems; // Start with all items
+    _searchController.addListener(() {
+      filterItems();
+    });
+  }
+
+  void filterItems() {
+    String query = _searchController.text.toLowerCase();
+    setState(() {
+      filteredItems = foodItems
+          .where((item) => item['name']!.toLowerCase().contains(query))
+          .toList();
+    });
+  }
+
+  @override
+  void dispose() {
+    _searchController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -15,7 +66,6 @@ class FoodFacts extends StatelessWidget {
             fontWeight: FontWeight.bold,
           ),
         ),
-        centerTitle: true,
       ),
       body: SingleChildScrollView(
         padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 16.0),
@@ -24,6 +74,7 @@ class FoodFacts extends StatelessWidget {
           children: [
             SizedBox(height: 16),
             TextField(
+              controller: _searchController,
               decoration: InputDecoration(
                 prefixIcon: Icon(Icons.search),
                 hintText: "Search for a product",
@@ -72,52 +123,44 @@ class FoodFacts extends StatelessWidget {
               ),
             ),
             SizedBox(height: 16),
-            GridView.count(
+            GridView.builder(
               shrinkWrap: true,
               physics: NeverScrollableScrollPhysics(),
-              crossAxisCount: 3, // 3 cards in a row
-              crossAxisSpacing: 16,
-              mainAxisSpacing: 16,
-              children: [
-                {'name': 'Ale beer', 'image': 'assets/images/favicon.png'},
-                {'name': 'Alfredo sauce', 'image': 'assets/images/favicon.png'},
-                {'name': 'Allspice', 'image': 'assets/images/favicon.png'},
-                {'name': 'Almond milk', 'image': 'assets/images/favicon.png'},
-                {'name': 'Almonds', 'image': 'assets/images/favicon.png'},
-                {
-                  'name': 'American blueberries',
-                  'image': 'assets/images/favicon.png'
-                },
-              ]
-                  .map(
-                    (item) => Column(
-                      children: [
-                        Expanded(
-                          child: Container(
-                            decoration: BoxDecoration(
-                              color: Colors.grey[100],
-                              borderRadius: BorderRadius.circular(8),
-                            ),
-                            child: Center(
-                              child: Image.asset(
-                                item['image'] as String,
-                                fit: BoxFit.cover,
-                                width: 60,
-                                height: 60,
-                              ),
-                            ),
+              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 3, // 3 cards in a row
+                crossAxisSpacing: 16,
+                mainAxisSpacing: 16,
+              ),
+              itemCount: filteredItems.length,
+              itemBuilder: (context, index) {
+                var item = filteredItems[index];
+                return Column(
+                  children: [
+                    Expanded(
+                      child: Container(
+                        decoration: BoxDecoration(
+                          color: const Color.fromARGB(255, 169, 141, 144),
+                          borderRadius: BorderRadius.circular(8),
+                        ),
+                        child: Center(
+                          child: Image.asset(
+                            item['image']!,
+                            fit: BoxFit.cover,
+                            width: 50,
+                            height: 50,
                           ),
                         ),
-                        SizedBox(height: 8),
-                        Text(
-                          item['name'] as String,
-                          style: TextStyle(fontSize: 14),
-                          textAlign: TextAlign.center,
-                        ),
-                      ],
+                      ),
                     ),
-                  )
-                  .toList(),
+                    SizedBox(height: 8),
+                    Text(
+                      item['name']!,
+                      style: TextStyle(fontSize: 14),
+                      textAlign: TextAlign.center,
+                    ),
+                  ],
+                );
+              },
             ),
           ],
         ),
